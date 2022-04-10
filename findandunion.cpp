cc720d1e1a3304362693a51de6be16e569e6e67f
ReadHegel <98605742+ReadHegel@users.noreply.github.com>
2022-04-10 10:11:05 +0200

    Update findandunion.cpp

--- findandunion.cpp
+++ findandunion.cpp
@@ -2,42 +2,29 @@
 
 using namespace std;
 
-const int Size = 1000005;
 struct FandU{
 	
-	int oj[Size];
-	int roz[Size];
+	vector<int> ojciec;
+	vector<int> roz;
 	
-	void initiation(){
-		for(int i=0; i<Size; i++){
-			roz[i]++;	
-			oj[i]=i;
-		}
+	FandU(int n){
+		ojciec.resize(n+1); roz.resize(n+1);
+		for(int i=0; i<n+1; i++){roz[i]++; ojciec[i]=i;}
 	}
 	
 	int F(int v){
-		if(oj[v]!=v){
-			int wynik=F(oj[v]);
-			oj[v]=wynik;
-			return wynik;
-		}
-		else return v;	
+		return v == ojciec[v] ? v : ojciec[v] = F(ojciec[v]);
 	}
 
 	void U(int a, int b){
 		a = F(a); b = F(b);
-		if(roz[a] < roz[b]){
-			roz[b]+=roz[a];
-			oj[a]=b;
-		}
-		else{
-			roz[a]+=roz[b];
-			oj[b]=a;
-		}
+		if(roz[a] < roz[b])swap(a , b);
+		roz[a]+=roz[b];
+		ojciec[b]=a;
 	}
 };
 
-FandU x;
+FandU FU(5);
 
 int main(){
 	std::ios_base::sync_with_stdio(0);
